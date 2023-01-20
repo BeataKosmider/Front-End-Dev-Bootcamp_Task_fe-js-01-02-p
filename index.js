57ab2961f07765232ab80bd697b0833e6e10ff62
Beata Kosmider <kosmibe@gmail.com>
2023-01-20 14:51:49 +0100

    task is done

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Zaimplementuj zadanie zgodnie z wytycznymi
-const car = {};
+const car = {
+  name: "Toyota Yaris",
+  price: 50000,
+  led: true,
+  color: "blue",
+  numbers: [1, 2, 3],
+};
 
 module.exports = car;
